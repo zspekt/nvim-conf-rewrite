20879638f170eaf57828ce95513529289c3062c1
zspekt <zspekt@tutanota.com>
2024-06-30 16:09:54 -0300

    oil fix
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -13,26 +13,13 @@ return {
         keymaps = {
           ["<C-h>"] = false,
           ["<M-h>"] = "actions.select_split",
-          ["<leader>gd"] = {
-            function()
-              local oil = require "oil"
-              local config = require "oil.config"
-              if #config.columns == 1 then
-                oil.set_columns { "icon", "permissions", "size", "mtime" }
-              else
-                oil.set_columns { "icon" }
-              end
-            end,
-            "Toggle detailed view",
-          },
+          -- [""] = ,
         },
         view_options = {
           show_hidden = true,
         },
       }
-
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-        vim.keymap.set("n", "<leader>-", require("oil").toggle_float) -- , { desc = "Open parent directory in floating" })
     end,
   
   },
